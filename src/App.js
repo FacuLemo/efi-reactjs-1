b31880ae82bb28524b73d820274350c9a8b2dcb6
Facundo Esteban Lemo <facundolemo@gmail.com>
2023-10-05 20:03:21 -0300

    linted code

--- src/App.js
+++ src/App.js
@@ -1,68 +1,66 @@
+import Drawclass from './components/drawclass/Drawclass'
+import Menu from './components/menu/Menu'
 
-import Drawclass from "./components/drawclass/Drawclass"
-import Menu from "./components/menu/Menu";
-
-function App() {
-  
+function App () {
   const configMenu = {
     configColor: {
-    background:'#f4f5fa', // Color de Fondo General de la botonera
-    itemBackground: '#d0d0d0',// Color de Fondo de los subMenús
-    itemColor:'#666', // Color del texto de cada item del menú
-    itemActive:'#a8a8a8', // Color cuando hace click y se abre un submenú
+      background: '#f4f5fa', // Color de Fondo General de la botonera
+      itemBackground: '#d0d0d0', // Color de Fondo de los subMenús
+      itemColor: '#666', // Color del texto de cada item del menú
+      itemActive: '#a8a8a8' // Color cuando hace click y se abre un submenú
     },
-    idFirstNivel : 150,
-    menuItems : [
-    {name:'Action', isFolder:false, id:148, idPadre:150},
-    {name:'Folder', isFolder:true, id:2, idPadre:150},
-    {name:'Subaction', isFolder:false, id:3, idPadre:2},
-    {name:'Subfolder', isFolder:true, id:4, idPadre:2},
-    {name:'Subfolder', isFolder:true, id:5, idPadre:2},
-    {name:'Sub-subaction', isFolder:false, id:55, idPadre:5},
-    {name:'Sub-subaction', isFolder:false, id:56, idPadre:5},
-    {name:'Sub-subaction', isFolder:false, id:59, idPadre:4},
-    ],
-    } 
+    idFirstNivel: 150,
+    menuItems: [
+      { name: 'Action', isFolder: false, id: 148, idPadre: 150 },
+      { name: 'Folder', isFolder: true, id: 2, idPadre: 150 },
+      { name: 'Subaction', isFolder: false, id: 3, idPadre: 2 },
+      { name: 'Subfolder', isFolder: true, id: 4, idPadre: 2 },
+      { name: 'Subfolder', isFolder: true, id: 5, idPadre: 2 },
+      { name: 'Sub-subaction', isFolder: false, id: 55, idPadre: 5 },
+      { name: 'Sub-subaction', isFolder: false, id: 56, idPadre: 5 },
+      { name: 'Sub-subaction', isFolder: false, id: 59, idPadre: 4 }
+    ]
+  }
 
-    const configUml = {
+  const configUml = {
     name: 'Person',
-    attributes: ['+name:str','+phoneNumber:str','+emailAddress:str'],
-    methods:[],
-    borderColor:'#e83838',
-    headColor:'rgb(253, 197, 197)',
-    textColor:'#333333'
-}
+    attributes: ['+name:str', '+phoneNumber:str', '+emailAddress:str'],
+    methods: [],
+    borderColor: '#e83838',
+    headColor: 'rgb(253, 197, 197)',
+    textColor: '#333333'
+  }
   const configUml2 = {
     name: 'Vehicle',
-    attributes: ['+brand:str','+idNumber:str','+model:str','+year:int'],
-    methods:['+forward()','+brake','+turnLeft()','+turnRight()'],
-    borderColor:'#34afe0',
-    headColor:'#84c1d9',
-    textColor:'#333333'
+    attributes: ['+brand:str', '+idNumber:str', '+model:str', '+year:int'],
+    methods: ['+forward()', '+brake', '+turnLeft()', '+turnRight()'],
+    borderColor: '#34afe0',
+    headColor: '#84c1d9',
+    textColor: '#333333'
   }
   const configUml3 = {
     name: 'Employee',
-    attributes: ['+position:str','+department:str','+salary:int','+yearOfEntry:int'],
-    methods:['+work()','+changePosition()'],
-    borderColor:'#fcba03',
-    headColor:'#ffe499',
-    textColor:'#333333'
+    attributes: ['+position:str', '+department:str', '+salary:int', '+yearOfEntry:int'],
+    methods: ['+work()', '+changePosition()'],
+    borderColor: '#fcba03',
+    headColor: '#ffe499',
+    textColor: '#333333'
   }
 
   return (
     <div>
       <header>
-    
+
       <Menu data={configMenu}/>
-  
+
       <Drawclass data={configUml}/>
       <Drawclass data={configUml2}/>
       <Drawclass data={configUml3}/>
       <Drawclass/>
-     
+
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/drawclass/Drawclass.js
+++ src/components/drawclass/Drawclass.js
@@ -1,18 +1,17 @@
-import { Component } from "react";
-import "./Drawclass.css";
+import { Component } from 'react'
+import './Drawclass.css'
 
-class Drawclass extends Component{
-
-    render(){
-        let svgWidth = this.getLongerWord()* 11; // Multiplico la cantidad de caracteres x 11 pixeles (ancho promedio de cada caracter)
-        let bodyHeight = (this.props.data.attributes.length + this.props.data.methods.length)* 35; /* Sumo la cant de atributos y metodos y mult. 
-        x 35 estimando que ocupan 35 px de alto c/u.*/
-        let headerHeight = 40; 
-        let lineHeightY = this.props.data.attributes.length * 35 + 37; // Cálculo de linea divisoria.                   
-        if(this.props.data.methods.length === 0){
-            bodyHeight += 35; // Si no hay métodos, se deja un gap.
-        } 
-        return(
+class Drawclass extends Component {
+  render () {
+    const svgWidth = this.getLongerWord() * 11 // Multiplico la cantidad de caracteres x 11 pixeles (ancho promedio de cada caracter)
+    let bodyHeight = (this.props.data.attributes.length + this.props.data.methods.length) * 35 /* Sumo la cant de atributos y metodos y mult.
+        x 35 estimando que ocupan 35 px de alto c/u. */
+    const headerHeight = 40
+    const lineHeightY = this.props.data.attributes.length * 35 + 37 // Cálculo de linea divisoria.
+    if (this.props.data.methods.length === 0) {
+      bodyHeight += 35 // Si no hay métodos, se deja un gap.
+    }
+    return (
             <svg overflow="visible" width={svgWidth} height={bodyHeight + headerHeight}>
                 <rect stroke={this.props.data.borderColor} x="0" y="0" width={svgWidth} height={headerHeight} fill={this.props.data.headColor}/>
                 <rect stroke={this.props.data.borderColor} x="0" y="40" width={svgWidth} height={bodyHeight} fill="white"/>
@@ -20,53 +19,53 @@ class Drawclass extends Component{
                 {this.getAttributesAndMethods()}
                 <line stroke="black" x1={0} y1={lineHeightY} x2={svgWidth} y2={lineHeightY}/>
             </svg>
-        )
-    }
+    )
+  }
 
-    getLongerWord(){
-        let attributes = this.props.data.attributes;
-        let methods = this.props.data.methods;
-        let classname = this.props.data.name;
-        let content = attributes.concat(methods).concat(classname);
-        let greaterWidth = 0;
-        for(let i = 0; i < content.length; i++){
-            if(content[i].length > greaterWidth){
-                greaterWidth = content[i].length
-            }
-        }
-        return greaterWidth;
+  getLongerWord () {
+    const attributes = this.props.data.attributes
+    const methods = this.props.data.methods
+    const classname = this.props.data.name
+    const content = attributes.concat(methods).concat(classname)
+    let greaterWidth = 0
+    for (let i = 0; i < content.length; i++) {
+      if (content[i].length > greaterWidth) {
+        greaterWidth = content[i].length
+      }
     }
+    return greaterWidth
+  }
 
-    getAttributesAndMethods(){
-        let height = 25;
-        let attributes = this.props.data.attributes
-        let methods = this.props.data.methods
-        if(attributes.length > 0){
-            attributes = attributes.map((item,index) => (
-                <text key={index} x={10} y={height +=35}>{item}</text>
-            ))
-        }
-        if(methods.length > 0){
-            methods = methods.map((item,index) => (
-                <text key={index} x={10} y={height +=35}>{item}</text>
-            ))
-        } 
-        return (<svg> 
+  getAttributesAndMethods () {
+    let height = 25
+    let attributes = this.props.data.attributes
+    let methods = this.props.data.methods
+    if (attributes.length > 0) {
+      attributes = attributes.map((item, index) => (
+                <text key={index} x={10} y={height += 35}>{item}</text>
+      ))
+    }
+    if (methods.length > 0) {
+      methods = methods.map((item, index) => (
+                <text key={index} x={10} y={height += 35}>{item}</text>
+      ))
+    }
+    return (<svg>
             {attributes}
             {methods}
         </svg>)
-    }
+  }
 }
 
 Drawclass.defaultProps = {
-    data:{
-        name: 'Classname',
-        attributes: ['+attribute:type'],
-        methods:['+method()'],
-        borderColor:'#e83838',
-        headColor:'rgb(253, 197, 197)',
-        textColor:'#333333'
-    }
+  data: {
+    name: 'Classname',
+    attributes: ['+attribute:type'],
+    methods: ['+method()'],
+    borderColor: '#e83838',
+    headColor: 'rgb(253, 197, 197)',
+    textColor: '#333333'
+  }
 }
 
 export default Drawclass
